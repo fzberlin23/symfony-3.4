5a392e3e7ea45ff9cadee51b3e17ff2debe1f6d0
Fabian Ziebig <ziebig@bigmind.de>
2019-01-31 17:13:57 +0100

    TicTacToe Game

--- symfony/web/js/ticTacToe.js
+++ symfony/web/js/ticTacToe.js
@@ -6,69 +6,81 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Square = function (_React$Component) {
-    _inherits(Square, _React$Component);
+function Square(props) {
+    return React.createElement(
+        'button',
+        { className: 'square', onClick: props.onClick },
+        props.value
+    );
+}
 
-    function Square() {
-        _classCallCheck(this, Square);
+var Board = function (_React$Component) {
+    _inherits(Board, _React$Component);
 
-        return _possibleConstructorReturn(this, (Square.__proto__ || Object.getPrototypeOf(Square)).apply(this, arguments));
-    }
-
-    _createClass(Square, [{
-        key: "render",
-        value: function render() {
-            return React.createElement("button", { className: "square" });
-        }
-    }]);
-
-    return Square;
-}(React.Component);
-
-var Board = function (_React$Component2) {
-    _inherits(Board, _React$Component2);
-
-    function Board() {
+    function Board(props) {
         _classCallCheck(this, Board);
 
-        return _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
+
+        _this.state = {
+            squares: Array(0).fill(null),
+            xIsNext: true
+        };
+        return _this;
     }
 
     _createClass(Board, [{
-        key: "renderSquare",
+        key: 'renderSquare',
         value: function renderSquare(i) {
-            return React.createElement(Square, null);
+            var _this2 = this;
+
+            return React.createElement(Square, {
+                value: this.state.squares[i],
+                onClick: function onClick() {
+                    return _this2.handleClick(i);
+                }
+            });
         }
     }, {
-        key: "render",
+        key: 'handleClick',
+        value: function handleClick(i) {
+            var squares = this.state.squares.slice();
+            squares[i] = this.state.xIsNext ? 'X' : 'O';
+            this.setState({
+                squares: squares,
+                xIsNext: !this.state.xIsNext
+            });
+        }
+    }, {
+        key: 'render',
         value: function render() {
             var status = 'Next player: X';
 
             return React.createElement(
-                "div",
+                'div',
                 null,
                 React.createElement(
-                    "div",
-                    { className: "status" },
+                    'div',
+                    { className: 'status' },
                     status
                 ),
                 React.createElement(
-                    "div",
-                    { className: "board-row" },
+                    'div',
+                    { className: 'board-row' },
                     this.renderSquare(0),
                     this.renderSquare(1),
                     this.renderSquare(2)
                 ),
                 React.createElement(
-                    "div",
-                    { className: "board-row" },
+                    'div',
+                    { className: 'board-row' },
                     this.renderSquare(3),
                     this.renderSquare(4),
                     this.renderSquare(5)
                 ),
                 React.createElement(
-                    "div",
-                    { className: "board-row" },
+                    'div',
+                    { className: 'board-row' },
                     this.renderSquare(6),
                     this.renderSquare(7),
                     this.renderSquare(8)
@@ -80,8 +92,8 @@ var Board = function (_React$Component2) {
     return Board;
 }(React.Component);
 
-var Game = function (_React$Component3) {
-    _inherits(Game, _React$Component3);
+var Game = function (_React$Component2) {
+    _inherits(Game, _React$Component2);
 
     function Game() {
         _classCallCheck(this, Game);
@@ -90,21 +102,21 @@ var Game = function (_React$Component3) {
     }
 
     _createClass(Game, [{
-        key: "render",
+        key: 'render',
         value: function render() {
             return React.createElement(
-                "div",
-                { className: "game" },
+                'div',
+                { className: 'game' },
                 React.createElement(
-                    "div",
-                    { className: "game-board" },
+                    'div',
+                    { className: 'game-board' },
                     React.createElement(Board, null)
                 ),
                 React.createElement(
-                    "div",
-                    { className: "game-info" },
-                    React.createElement("div", null),
-                    React.createElement("ol", null)
+                    'div',
+                    { className: 'game-info' },
+                    React.createElement('div', null),
+                    React.createElement('ol', null)
                 )
             );
         }
--- symfony/web/src/ticTacToe.js
+++ symfony/web/src/ticTacToe.js
@@ -1,39 +1,61 @@
-class Square extends React.Component {
-    render() {
-        return (
-        <button className="square">
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
-        );
-    }
+    );
 }
 
 class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(0).fill(null),
+            xIsNext: true
+        };
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     render() {
         const status = 'Next player: X';
 
         return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
         );
     }
 }
@@ -41,15 +63,15 @@ class Board extends React.Component {
 class Game extends React.Component {
     render() {
         return (
-        <div className="game">
-            <div className="game-board">
-            <Board />
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div></div>
+                    <ol></ol>
+                </div>
             </div>
-            <div className="game-info">
-            <div></div>
-            <ol></ol>
-            </div>
-        </div>
         );
     }
 }
