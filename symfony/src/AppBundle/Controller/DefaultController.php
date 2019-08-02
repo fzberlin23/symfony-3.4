01920bd6e9b8d930c91f34e4fc2697df9063286a
Fabian Ziebig <4408553+fzberlin23@users.noreply.github.com>
2019-08-02 08:15:49 +0200

    removed unused imports

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -4,8 +4,6 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 use AppBundle\Entity\Product;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
